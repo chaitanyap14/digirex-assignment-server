5179a67137f38a0e2538d542d4e90a31a2009c10
Chaitanya Palghadmal <cpalghadmal14@gmail.com>
2022-02-07 19:43:41 +0530

    Initial server deploy on heroku

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const jwtGen = require("./utils/jwtGen");
 const pool = require("./db");
-const port = 5000;
+const port = process.env.PORT || 5003;
 
 app.use(cors({ origin: true, credentials: true }));
 app.use(express.json());
@@ -15,6 +15,23 @@ app.use(
     extended: false,
   })
 );
+
+//register
+app.post("/register", (req, res) => {
+  const data = req.body.data;
+  const passHash = bcrypt.hashSync(data.pass, 10);
+  pool.query(
+    `INSERT INTO Pharmacist VALUES ("${data.name}", "${data.email}", "${data.address}", "${data.mobile}", "${data.date}", "${passHash}");`,
+    (error, results, fields) => {
+      if (error) {
+        console.log(error);
+        res.send({ status: false });
+      } else {
+        res.send({ status: true });
+      }
+    }
+  );
+});
 
 app.get("/", (req, res) => {
   res.send("Server Running");
