8755cfbcb35de18c7258631eec72db91d7e8c70c
Chaitanya Palghadmal <cpalghadmal14@gmail.com>
2022-02-07 18:01:56 +0530

    Added env file

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
 require("dotenv").config();
 const { Pool } = require("pg");
 const pool = new Pool({
-  host: "localhost",
-  user: "postgres",
-  database: "medrecord",
-  port: 5432,
-  password: "root1412",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  database: process.env.DB_DB,
+  port: process.env.DB_PORT,
+  password: process.env.DB_PASS,
 });
 
 module.exports = pool;
